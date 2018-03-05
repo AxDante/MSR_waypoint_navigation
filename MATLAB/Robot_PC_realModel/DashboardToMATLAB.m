8b42c1331175dab563e3299927c84e34a1eb1bab
IceFox <knight16729438@gmail.com>
2018-03-06 02:09:42 +0800

    field test preperation

--- MATLAB/Robot_PC_realModel/DashboardToMATLAB.m
+++ MATLAB/Robot_PC_realModel/DashboardToMATLAB.m
@@ -1,29 +1,43 @@
-%{
-for intidx = 1:100
-    fid = fopen('C:\Marvelmind\dashboard\logs\hedgehogs.log','rt');
-    A = textscan(fid, '%f %f', 'Delimiter', ',' , 'collectoutput', true, 'HeaderLines', );
-    x(intidx) = A(4)
-    y(intidx) = A(5)
-    pause(2);
-    fclose(fid);
-end
-%}
-LastNewA = [0.1 0.1];
+
+txt_endLine_new = [0 0];
+
+is_initialized = false;
+is_grid_on = true;
+
+
 figure(1)
 hold on
+axis([-grid_w grid_w*11 -grid_w grid_w*11])
+if (is_grid_on)
+    for idxx = 1:(grid_size(1) + 1)
+        line(grid_w*[(idxx-1) (idxx-1)], grid_w*[0 grid_size(2)], 'Color', 'black');
+    end
+    for idxy = 1:(grid_size(2) + 1)
+        line(grid_w*[0 grid_size(1)], grid_w*[(idxy-1) (idxy-1)], 'Color', 'black');
+    end
+end
+
+
 for intidx = 1:400000
     fid = fopen('C:\Marvelmind\dashboard\logs\TestLog.txt','rt');
-    A = textscan(fid, '%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f', 'delimiter', ',','collectoutput',true...
+    txt_Streaming = textscan(fid, '%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f', 'delimiter', ',','collectoutput',true...
           , 'HeaderLines', 10);
-    A=A{1};
+    txt_Streaming=txt_Streaming{1};
     fclose(fid);
-    n=size(A,1);
-    if  (NewA(1) ~= 0 &&  ~isnan(NewA(1))  &&  NewA(2) ~= 0 &&  ~isnan(NewA(2)) )
-        if (LastNewA(1) ~= NewA(1) || LastNewA(2) ~= NewA(2)) 
-            line([LastNewA(1) NewA(1)], [LastNewA(2) NewA(2)]);
+    txt_rows=size(txt_Streaming,1);
+    txt_endLine = txt_Streaming(end, 5:6);
+    
+    if (~is_initialized && (txt_endLine(1) ~= 0 &&  ~isnan(txt_endLine(1))  &&  txt_endLine(2) ~= 0 &&  ~isnan(txt_endLine(2))))
+        txt_endLine_new = txt_endLine;
+        is_initialized = true;
+    elseif  (txt_endLine(1) ~= 0 &&  ~isnan(txt_endLine(1))  &&  txt_endLine(2) ~= 0 &&  ~isnan(txt_endLine(2)) )
+        if (txt_endLine_new(1) ~= txt_endLine(1) || txt_endLine_new(2) ~= txt_endLine(2)) 
+            if norm(txt_endLine_new - txt_endLine) < 0.4
+                line([txt_endLine_new(1) txt_endLine(1)], [txt_endLine_new(2) txt_endLine(2)]);
+                txt_endLine_new = txt_endLine;
+            end
         end
-        LastNewA = NewA;
     end
-    pause(0.5)
+    pause(0.3)
 end
 
