b457da0fae81a52aec4342c6b4d1a569b1338af2
DESKTOP-A5FBOJD\pppp8 <knight16729438@gmail.com>
2018-09-27 16:26:59 +0800

    MATLAB Tiling Planning v1.01: Adding simulating

---
+++ MATLAB/Robot/SequencingSim_TSP/alg_random_path.m
@@ -0,0 +1,17 @@
+function [Wp_out, total_dis] = alg_random_path(Wp, rng_seed)
+
+    rand('seed', rng_seed);
+    
+    count_wp = size(Wp,1);
+    rng_array = randperm(count_wp-1)+1;
+    
+    Wp_out(1,:) = Wp(1,:);
+    total_dis = 0;
+    
+    for intidx = 1:count_wp-1
+        Wp_out(intidx+1,:) = Wp(rng_array(intidx), :);
+        dis = abs(Wp_out(end, 1) - Wp_out(end-1,1)) + abs(Wp_out(end, 2) - Wp_out(end-1,2));
+        total_dis = total_dis + dis;
+    end
+
+end
