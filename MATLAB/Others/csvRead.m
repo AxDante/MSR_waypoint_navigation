06809674ccf5415e17b8513ccfa747fb7647582d
Ping-Cheng <knight16729438@gmail.com>
2020-10-10 01:09:29 -0400

    Update code and clean up directories

--- MATLAB/Others/csvRead.m
+++
@@ -1,4 +0,0 @@
-
-addpath('C:\Users\IceFox\Desktop\ERMINE\MATLAB\Robot_PC')
-addpath('C:\Users\IceFox\Desktop\ERMINE\MATLAB\Robot_PC\Maps')
-M = csvread('10_10_obs.txt');
--- MATLAB/Robot/SequencingSim - Copy/rotationMatrix.m
+++
@@ -1,8 +0,0 @@
-function R = rotationMatrix(M, config)
-    shape = mod(config-1, 7)+1;
-    deg = pi/2*floor((config-1)/7);
-    rotation_matrix = [cos(deg) -sin(deg);
-                sin(deg) cos(deg)];
-    full_rotation = M*rotation_matrix;
-    R = round(full_rotation(shape*3-2:shape*3, :));
-end
